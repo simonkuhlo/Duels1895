71a6ed1f716a6b9b99e0e14ec2374fff67d80143
Simon <siga241@gmail.com>
2025-09-06 18:44:09 +0200

    add player character body

---
+++ Game/Entities/player_character_body_3d.gd
@@ -0,0 +1,8 @@
+extends EntityBody3D
+class_name PlayerCharacterBody3D
+
+@export_group("Linking")
+@export var action_stamina:EntityStatHolder
+@export var movement_stamina:EntityStatHolder
+@export var ads_stamina:EntityStatHolder
+@export var controller:Node
