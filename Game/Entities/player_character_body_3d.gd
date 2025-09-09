54fa13388d0417e6f66d49a89b546b293a3c602e
Simon <siga241@gmail.com>
2025-09-09 15:08:04 +0200

    update body scenes

--- Game/Entities/player_character_body_3d.gd
+++ Game/Entities/player_character_body_3d.gd
@@ -5,4 +5,18 @@ class_name PlayerCharacterBody3D
 @export var action_stamina:EntityStatHolder
 @export var movement_stamina:EntityStatHolder
 @export var ads_stamina:EntityStatHolder
+<<<<<<< Updated upstream
 @export var controller:Node
+=======
+@export var jump_strength:EntityStatHolder
+@export var controller:BasePlayerCharacterController:
+	set(new):
+		controller = new
+		if controller:
+			controller.controlled_entity = self
+
+func _physics_process(delta: float) -> void:
+	if is_multiplayer_authority():
+		if controller:
+			controller._on_controlled_entity_physics_process(delta)
+>>>>>>> Stashed changes
