41640f9794fb96f7e32e7c35b8e22fc211dd79c5
Simon <siga241@gmail.com>
2025-09-11 22:46:41 +0200

    add database functionality

--- Game/Item System/item_reference_db.gd
+++ Game/Item System/item_reference_db.gd
@@ -1,4 +1,8 @@
+@tool
 extends Resource
 class_name ItemDB
 
-var content:Dictionary[StringName, ItemReference] = {}
+@export var content:Dictionary[StringName, ItemReference] = {}
+@export var add_item:ItemReference:
+	set(new):
+		content[new.id] = new
