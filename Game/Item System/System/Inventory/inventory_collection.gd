fba8cd348a8bba82173897bee63ed8ea86cf371e
Simon <108664817+simonkuhlo@users.noreply.github.com>
2025-09-15 09:51:44 +0200

    Update inventory_collection.gd
    Add add_item function

--- Game/Item System/System/Inventory/inventory_collection.gd
+++ Game/Item System/System/Inventory/inventory_collection.gd
@@ -14,3 +14,13 @@ func get_content(filter:BaseFilter = null) -> Array[ItemInstance]:
 	for inventory in inventories:
 		returned_array.append_array(inventory.get_content(filter))
 	return returned_array
+
+##Adds a given item to the first free inventoryslot in the collection.
+##If allow_split is true, it will fill already existing stacks first.
+func add_item(item:ItemInstance, allow_split:bool = true) -> ItemInstance:
+	var remaining_item:ItemInstance = item
+	for inventory in inventories:
+		remaining_item = inventory.add_item(item, allow_split)
+		if !remaining_item:
+			return
+	return remaining_item
