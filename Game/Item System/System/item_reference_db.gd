73f7c868ce9a2b43acb9f2032290a4446f151fe9
Simon <siga241@gmail.com>
2025-09-19 16:14:06 +0200

    add world item spawner

--- Game/Item System/System/item_reference_db.gd
+++ Game/Item System/System/item_reference_db.gd
@@ -2,7 +2,14 @@
 extends Resource
 class_name ItemDB
 
-@export var content:Dictionary[StringName, ItemReference] = {}
+
 @export var add_item:ItemReference:
 	set(new):
 		content[new.uid] = new
+		changed.emit()
+
+@export var default_world_item_scene:PackedScene
+@export var default_held_item_scene:PackedScene
+
+@export_group("System")
+@export var content:Dictionary[StringName, ItemReference] = {}
