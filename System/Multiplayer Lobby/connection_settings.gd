66b0a273d52e7c262e558c5713e25947b38c4c40
Simon <siga241@gmail.com>
2025-09-25 00:46:08 +0200

    edit ui

--- System/Multiplayer Lobby/connection_settings.gd
+++ System/Multiplayer Lobby/connection_settings.gd
@@ -3,9 +3,12 @@ class_name ConnectionSettings
 
 @export var game_port:int = 6969
 @export var used_profile:MultiplayerProfile = MultiplayerProfile.new()
+@export var discovery_server_address:String = "https://matchmaking.simonkuhlo.de"
 
 @export_group("Server only")
 @export var max_players:int = 10
+@export var lobby_title:String = "New Lobby"
+@export var lobby_password:String
 
 @export_group("Client only")
 @export var remote_address:String = "localhost"
