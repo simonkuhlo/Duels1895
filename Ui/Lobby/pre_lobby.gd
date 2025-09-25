30e492dd250a3b60b2926a86fcb1016b24dd0ac3
Gander Simon <siga241@gmail.com>
2025-09-25 14:25:52 +0200

    update lobby browser ui frame

--- Ui/Lobby/pre_lobby.gd
+++ Ui/Lobby/pre_lobby.gd
@@ -1,13 +1,10 @@
 extends Control
 
-@export var connection_settings_editor:Control
 @export var lobby_creator:Control
+@export var lobby_browser:Control
 
 func _on_join_button_pressed() -> void:
-	Lobby.create_client()
+	lobby_browser.show()
 
 func _on_create_button_pressed() -> void:
 	lobby_creator.show()
-
-func _on_connection_settings_pressed() -> void:
-	connection_settings_editor.show()
