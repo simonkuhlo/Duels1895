65ce5a244ddf0605163a87c9841e0979c086d18c
Simon <siga241@gmail.com>
2025-10-01 16:11:16 +0200

    add bullet_instance

---
+++ bullet_instance.gd
@@ -0,0 +1,17 @@
+extends RayCast3D
+
+func _physics_process(delta: float) -> void:
+	var ray_cast:RayCast3D = RayCast3D.new()
+	var pos_next:Vector3 = calculate_next_position(delta)
+	target_position = pos_next - global_transform.origin
+	var collider = get_collider()
+	if collider:
+		collide(collider)
+	global_transform.origin = pos_next
+	print(transform.origin)
+
+func calculate_next_position(delta:float) -> Vector3:
+	return global_transform.origin + Vector3.FORWARD * 10 * delta
+
+func collide(collider:Object) -> void:
+	pass
