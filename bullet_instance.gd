5fb204780034570aa80f8473c9ca56f5deef3b67
Simon <siga241@gmail.com>
2025-10-03 02:35:55 +0200

    fix bullet instance collision detection

--- bullet_instance.gd
+++ bullet_instance.gd
@@ -1,18 +1,22 @@
-extends RayCast3D
+extends Node3D
 class_name BulletInstance
 
 var damage_source:ProjectileDamageSource
 var ammo_reference:AmmoItem
-var current_velocity:float = 200
+var current_velocity:float = 10
 
 func _physics_process(delta: float) -> void:
-	var ray_cast:RayCast3D = RayCast3D.new()
-	var pos_next:Vector3 = calculate_next_position(delta)
-	target_position = pos_next - global_transform.origin
-	var collider = get_collider()
-	if collider:
+	var pos_now: Vector3 = global_transform.origin
+	var pos_next: Vector3 = calculate_next_position(delta)
+	var space_state = get_world_3d().direct_space_state
+	var query = PhysicsRayQueryParameters3D.create(pos_now, pos_next)
+	query.collide_with_areas = true
+	var result = space_state.intersect_ray(query)
+	if result:
+		var collider = result.collider
 		collide(collider)
 	global_transform.origin = pos_next
+
 
 func calculate_next_position(delta:float) -> Vector3:
 	return global_position - global_transform.basis.z * current_velocity * delta
